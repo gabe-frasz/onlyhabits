5605541adb72c99e90ea943ed73e4b02c7ec49bf
Gabriel <gabrielvitor.frasao@gmail.com>
2023-04-15 15:16:04 -0300

    refactor(web): uncomment logo

--- apps/web/src/components/layouts/Header/Header.tsx
+++ apps/web/src/components/layouts/Header/Header.tsx
@@ -1,16 +1,17 @@
 import { UserButton } from "@clerk/nextjs/app-beta";
 
+import { Logo } from "@/components/modules";
 import { NewHabitModal } from "./components";
 
 // TODO: Fix Heading component
 export const Header = () => {
   return (
     <header className="mx-auto flex w-full max-w-3xl flex-col items-center justify-between gap-4 sm:flex-row sm:gap-0">
-      {/* <Heading className="text-3xl font-bold" asChild>
-        <h1>
-          <Logo />
-        </h1>
-      </Heading> */}
+      {/* <Heading className="text-3xl font-bold" asChild> */}
+      <h1>
+        <Logo />
+      </h1>
+      {/* </Heading> */}
 
       <NewHabitModal />
 
