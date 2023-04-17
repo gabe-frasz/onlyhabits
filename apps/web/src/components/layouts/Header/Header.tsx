18bb33e56494c4ab445740a2ed5b6ee6c1a824be
Gabriel <gabrielvitor.frasao@gmail.com>
2023-04-17 19:20:23 -0300

    fix(web): fix Heading import error

--- apps/web/src/components/layouts/Header/Header.tsx
+++ apps/web/src/components/layouts/Header/Header.tsx
@@ -1,17 +1,17 @@
+import { Heading } from "@c6r/react";
 import { UserButton } from "@clerk/nextjs/app-beta";
 
 import { Logo } from "@/components/modules";
 import { NewHabitModal } from "./components";
 
-// TODO: Fix Heading component
 export const Header = () => {
   return (
     <header className="mx-auto flex w-full max-w-3xl flex-col items-center justify-between gap-4 sm:flex-row sm:gap-0">
-      {/* <Heading className="text-3xl font-bold" asChild> */}
-      <h1>
-        <Logo />
-      </h1>
-      {/* </Heading> */}
+      <Heading className="text-3xl font-bold" asChild>
+        <h1>
+          <Logo />
+        </h1>
+      </Heading>
 
       <NewHabitModal />
 
