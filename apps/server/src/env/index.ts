699e00d72e96c824365728db2292693d1dd168ee
Gabriel <gabrielvitor.frasao@gmail.com>
2023-05-08 19:29:10 -0300

    refactor(server): allow '*' as web origin environment variable

--- apps/server/src/env/index.ts
+++ apps/server/src/env/index.ts
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === "test") {
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "production", "test"]),
   PORT: z.coerce.number().default(3333),
-  WEB_ORIGIN_URL: z.string().url(),
+  WEB_ORIGIN_URL: z.string().url().or(z.literal("*")),
   DATABASE_URL: z.string().url(),
   CLERK_PUBLISHABLE_KEY: z.string(),
   CLERK_SECRET_KEY: z.string(),
