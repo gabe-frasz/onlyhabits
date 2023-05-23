0f1401c6bd7e1b542284fbb47c6c21ff9cd617f3
Gabriel <gabrielvitor.frasao@gmail.com>
2023-05-23 19:37:35 -0300

    build(web): update next to latest version

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,9 +3,6 @@ import "./src/env.mjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
 };
 
 export default nextConfig;
